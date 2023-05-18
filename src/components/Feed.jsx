708aa9dce8078c537f607808db37cee8f178cf24
Hemang Bairwa <69377228+hemang-2001@users.noreply.github.com>
2023-05-18 14:07:48 +0530

    API is addeded

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/icons-material";
 
 const Feed = () => {
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirction: { sx: "coloumn", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px soild #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        SideBar
+        <Typography className="copyright" variant="body2" sx= {{mt: 1.5, color: '#fff' }}>
+          CopyRight 2023 Hemang Bairwa
+        </Typography>
+    
+      </Box>
+    </Stack>
+  );
+};
 
 export default Feed
