1dee95fa09b70928fde85718590766689f1fb470
Hemang Bairwa <69377228+hemang-2001@users.noreply.github.com>
2023-07-19 00:30:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from "./App";    
 import './index.css';
-import ScrollToTop from "./helpers/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   <App />
+   <Router>
+      <App />
+   </Router>
 );
