620cf0bdaad62ad6666fee705355811939ec4b38
Hemang Bairwa <69377228+hemang-2001@users.noreply.github.com>
2024-10-12 19:05:47 +0530

    Some Feature Update

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
-import { Box, Typography } from "@mui/material";
-import { Videos } from "./";
+import { Box, Typography, Stack } from "@mui/material";
+import { SideBar, Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 import { useParams } from "react-router-dom";
-   
+import { categories } from '../utils/constants';
+
+
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState([]);
@@ -11,19 +13,44 @@ const SearchFeed = () => {
 
   useEffect(() => {
     fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
-    .then((data) => setVideos(data.items))
+      .then((data) => setVideos(data.items))
   }, [searchTerm]);
- 
 
-  return (
-    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white'}}>
-        Search Results for: <span style={{ color: '#F31503' }} >
-       {searchTerm} </span> videos
+
+    const [selectedCategory, setSelectedCategory] = useState(`New`);
+  
+
+
+return (
+
+
+  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+        <Box
+          sx={{
+            height: { sx: 'auto', md: '92vh' },
+            borderRight: "1px solid #3d3d3d",
+            px: { sx: 0, md: 2 }}}>
+  
+            <SideBar
+              selectedCategory= { selectedCategory }
+              setSelectedCategory= { setSelectedCategory } />
+  
+              <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
+              Copyright 2023 Hemang B.
+              </Typography>
+        </Box>
+    
+      <Box
+        p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+          Search Results for: <span style={{ color: '#F31503' }} >
+            {searchTerm} </span> videos
         </Typography>
         <Videos videos={videos} />
       </Box>
-  );
+      </Stack>
+  
+);
 };
 
-export default SearchFeed; 
+      export default SearchFeed; 
