7740bade109b63c72257b8c2b78b1ba36ee21bee
Hemang Bairwa <69377228+hemang-2001@users.noreply.github.com>
2024-12-07 10:59:02 +0530

    Alert Added to website App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { BrowserRouter, Routes, Route, Form } from "react-router-dom"
 import { Box } from '@mui/material';
 import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import Link from '@mui/material/Link' ;
+
+
 
 
 const App = () => (
     <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }}>
+
+    <Alert severity="warning">
+  <AlertTitle>Essive API Calls</AlertTitle>
+  Please Fork this Project on <Link href="https://github.com/hemang-2001/smartube">GitHub</Link> and add your own API key by steps shown in documentation if you use it ragularly. Star the repository too
+</Alert>
+
+
         <Navbar />
         <Routes>
            
